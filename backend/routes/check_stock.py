590c6dd0b68d00c474017f8e1732c95cb8f31ab3
Nashath Rasha <nashathrasha0@gmail.com>
2025-07-14 03:51:15 +0530

    feat(routes): implement /check-stock endpoint with SQLAlchemy ORM and session management

---
+++ backend/routes/check_stock.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from backend.db.database import sessionLocal, get_db
+from backend.db.schema import Stockrequest
+from backend.db.models import Product
+
+router = APIRouter()
+
+@router.post('/check-stock')
+def check_stock(data: Stockrequest, db: Session = Depends(get_db)):
+    product = db.query(Product).filter(Product.sku == data.sku).first()
+
+    if not product:
+        raise HTTPException(status_code=404, detail="product not found")
+    
+    return{
+        "sku": product.sku,
+        "name": product.name,
+        "available": product.quantity_available,
+        "price": product.price,
+        "in_stock": product.quantity_available >= data.quantity
+    }
+
